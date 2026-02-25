7eef7d1e730f0cedc766a8a2031bc39a59f340e1
Bijoy Chandra Roy <142526046+bijoy-chandra-roy@users.noreply.github.com>
2026-02-25 17:11:07 +0600

    fix: replaced <img> with <Image>

--- src/components/FoodCards.jsx
+++ src/components/FoodCards.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const FoodCards = ({ food }) => {
   const router = useRouter();
@@ -10,26 +11,26 @@ const FoodCards = ({ food }) => {
       className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden border-2 border-gray-100 p-2 cursor-pointer"
     >
       {/* Image */}
-      <figure className="">
-        <img
-          className="h-[150px] w-full flex object-cover hover:scale-110 transition duration-500 rounded-xl"
-          src={food.foodImg}
-          alt={food.title}
-          b
+      <figure className="overflow-hidden rounded-xl">
+        <Image
+          className="h-[150px] w-full flex object-cover hover:scale-110 transition duration-500"
+          src={food.foodImg || "https://via.placeholder.com/150"}
+          alt={food.title || "Food"}
+          width={400}
+          height={150}
         />
       </figure>
 
       {/* Content */}
-
       <div className="mt-2">
         <h2 className="font-semibold text-gray-800 mb-2">{food.title}</h2>
 
         <p className="text-gray-600">
-          <span className="font-medium text-gray-700">Category:</span>
+          <span className="font-medium text-gray-700">Category: </span>
           {food.category}
         </p>
 
-        <p className="mt-3 font-bold text-orange-500">${food.price}</p>
+        <p className="mt-3 font-bold text-orange-500">Tk {food.price}</p>
       </div>
     </div>
   );
