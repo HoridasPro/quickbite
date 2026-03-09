ff2b95b44d3a3584bd5684d45d0f428c111692a3
Bijoy Chandra Roy <142526046+bijoy-chandra-roy@users.noreply.github.com>
2026-03-09 17:06:42 +0600

    changed the export function name to proxy as well

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,7 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
+export async function proxy(req) {
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
@@ -9,7 +9,6 @@ export async function middleware(req) {
 
   const { pathname } = req.nextUrl;
 
-  // Aligning logic with the matcher in the config below
   if (pathname.startsWith("/dashboard/admin")) {
     if (!token || token.role !== "admin") {
       return NextResponse.redirect(new URL("/", req.url));
